ebd7597ae6e6e0c3f9e4d272c4bfb5dbb2fc4976
Nasim Akbor <nasimakbor@Nasims-Air.fios-router.home>
2022-03-31 20:52:04 -0400

    index route updated

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = Router();
 
 router.get("/", (req, res) => res.send("This is the api root!"));
 
-router.use("/", housesRoutes);
-router.use("/", charactersRoutes);
+router.use("/", digiRoutes);
+
 
 export default router;
