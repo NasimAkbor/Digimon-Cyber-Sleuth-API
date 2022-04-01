ab689a538de68d0717dfc8b30e7d6b7c3f384215
Nasim Akbor <nasimakbor@Nasims-Air.fios-router.home>
2022-03-31 20:49:27 -0400

    controllers set up

--- controllers/digimon-list.js
+++ controllers/digimon-list.js
@@ -1,1 +1,101 @@
+import { redirect } from "express/lib/response";
 import Digimon from "../models/digimon-list.js"
+
+export const getDigimon = async (req, res) => {
+  try {
+    const digimon = await Digimon.find();
+    res.json(digimon);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getDigimonName = async (req, res) => {
+  try {
+    let { name } = req.params;
+    const digimon = await Digimon.find({ Digimon: name })
+
+    if (digimon) {
+      return res.json(digimon);
+    }
+    res.status(400).json({ message: "Digimon not found!" })
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+};
+
+export const getDigimonAttribute = async (req, res) => {
+  try {
+    let { attribute } = req.params;
+    const digimon = await Digimon.find({ Attribute: attribute })
+
+    if (digimon) {
+      return res.json(digimon);
+    }
+    res.status(400).json({ message: "Digimon not found!" })
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+};
+
+export const getDigimonStage = async (req, res) => {
+  try {
+    let { stage } = req.params;
+    const digimon = await Digimon.find({ Stage: stage })
+
+    if (digimon) {
+      return res.json(digimon);
+    }
+    res.status(400).json({ message: "Digimon not found!" })
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+};
+
+export const getDigimonNumber = async (req, res) => {
+  try {
+    let { num } = req.params;
+    const digimon = await Digimon.find({ Number: num })
+
+    if (digimon) {
+      return res.json(digimon);
+    }
+    res.status(400).json({ message: "Digimon not found!" })
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+};
+
+export const createDigimon = async (req, res) => {
+  try {
+    const digimon = new Digimon(req.body);
+    await digimon.save();
+    res.status(201).json(digimon);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const updateDigimon = async (req, res) => {
+  const { name } = req.params;
+  const digimon = await House.findByIdAndUpdate(name, req.body);
+  res.status(200).json(digimon);
+};
+
+export const deleteDigimon = async (req, res) => {
+  try {
+    const { name } = req.params;
+    const deleted = await House.findByIdAndDelete(name);
+
+    if (deleted) {
+      return res.status(200).send("Digimon deleted!");
+    }
+
+    throw new Error("Digimon not found");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
